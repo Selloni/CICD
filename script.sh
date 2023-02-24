15bb5ece8637cfccaaef4a8f4959e76f4b22b9ba
Grandpat || Yakov <60509060+Selloni@users.noreply.github.com>
2023-02-24 15:01:37 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,2 +1,3 @@
- 
-scp src/cat/s21_cat src/grep/s21_grep move ya@192.168.100.10:/home/ya
+scp src/cat/s21_cat src/grep/s21_grep ya@192.168.100.10:/home/ya
+
+ssh ya@192.168.100.10 mv s21_cat s21_grep /usr/local/bin
